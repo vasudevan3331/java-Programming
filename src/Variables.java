f3a41c11bc2eaf63bd5fe4f326265a492ef9bb9f
vasudevan3331 <vasudevan3331@gmail.com>
2023-09-16 17:51:32 +0530

    first change

--- src/Variables.java
+++ src/Variables.java
@@ -13,12 +13,14 @@ public class Variables
     {
         String st="hello";
         int i=20;
+        long l=7894544L;
         float j=30F;
         char ch='a';
         boolean choose=false;
 
         System.out.println("name "+st);
         System.out.println("int "+i);
+        System.out.println("long "+l);
         System.out.println("float "+j);
         System.out.println("char "+ch);
         System.out.println("boolean "+choose);
