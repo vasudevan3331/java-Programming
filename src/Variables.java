5119c97ef1ef980e08da78b669cf6c0784fd4e62
vasudevan3331 <vasudevan3331@gmail.com>
2023-09-16 17:46:01 +0530

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,28 @@
+
+// Variables in Java
+/*
+    int
+    float
+    char
+    boolean
+ */
+
+public class Variables
+{
+    public static void main (String args[])
+    {
+        String st="hello";
+        int i=20;
+        float j=30F;
+        char ch='a';
+        boolean choose=false;
+
+        System.out.println("name "+st);
+        System.out.println("int "+i);
+        System.out.println("float "+j);
+        System.out.println("char "+ch);
+        System.out.println("boolean "+choose);
+
+    }
+
+}
